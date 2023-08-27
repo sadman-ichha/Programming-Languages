1d80d8ffc506a352503eef70c2b38550b47c7227
sadman_ichha <sadmanichha1@gmail.com>
2023-08-27 23:49:59 +0600

    variable

---
+++ 03. variable/variable.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void main()
+{
+    int a = 10;
+    int b = 20;
+    int c;
+    c = 50;
+
+    printf("%d + %d equlas %d \n", a, b, a + b);
+    printf("%d - %d equlas %d \n", a, b, a - b);
+    printf("%d * %d equlas %d \n", a, b, a * b);
+    printf("%d / %d equlas %d \n", a, b, a / b);
+}
