9daa491ca839afbf627c6279d1e8ef0adec2e1bc
sadman_ichha <sadmanichha1@gmail.com>
2023-09-02 11:47:44 +0600

    string

--- 07. String/string.c
+++ 07. String/string.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 int main(){
+    //approach one
     char myName[10];
     myName[0]= 'S'; 
     myName[1]='A';
@@ -8,8 +9,15 @@ int main(){
     myName[3]='M';
     myName[4]='A';
     myName[5]='N';
-    // %s is char Indentifier
-    printf("%s \n", myName);
+    // %s is char Identifier
+    printf("myName:  %s \n", myName);
+
+    //approach two
+    char myName2[20]={'S','A','D','M','A','N'};
+    printf("myName two: %s \n",myName2);
+   //approach three
+    char myName3[20]="Sadman";
+    printf("myName three: %s \n",myName3);
 
     return 0;
 }
