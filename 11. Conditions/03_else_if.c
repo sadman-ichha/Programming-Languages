e09f5e3fe5b5d9d0f4bb65ada8f93873401ff97b
sadman_ichha <sadmanichha1@gmail.com>
2023-09-09 23:22:59 +0600

    else if statement

--- 11. Conditions/03_else_if.c
+++ 11. Conditions/03_else_if.c
@@ -2,18 +2,20 @@
 
 int main()
 {
-
     int sadman = 20;
-    int jone = 10;
+    int jone = 20;
     // This is if else statement
     if (sadman > jone)
     {
         printf("Sadman is Elder! \n");
     }
-    else
+    else if (jone > sadman)
     {
         printf("Jone is elder!\n ");
     }
-
+    else
+    {
+        printf("Nothing else");
+    }
     return 0;
 }
