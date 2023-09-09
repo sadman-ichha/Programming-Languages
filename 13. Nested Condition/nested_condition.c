7e01cc5fb4e38229f155960dea73723ae6bee0ec
sadman_ichha <sadmanichha1@gmail.com>
2023-09-10 00:14:06 +0600

    nested condition

---
+++ 13. Nested Condition/nested_condition.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdbool.h>
+int main()
+{
+    int age = 19;
+    bool has_nid = false;
+    bool has_passport = false;
+
+    if (age >= 18){
+        // this called nested condition
+        if (has_nid || has_passport){
+            printf("Okey Let's go \n");
+        }
+        else{
+            printf("You're Not Accepted \n");
+        }
+    }
+    else{
+        printf("You're Not Allowed \n");
+    }
+
+    return 0;
+}
