03eccd9d4e26de38e282fcfc619483c341862c58
sadman_ichha <sadmanichha1@gmail.com>
2023-09-10 09:54:42 +0600

    uppercase & lowercase

---
+++ 15. Uppercase_Lowercase/uppercase_lowercase.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    char a;
+    printf("Enter a character: ");
+    scanf("%c", &a);
+
+    if (a >= 48 && a <= 57) {
+        printf("%c is a digit.\n", a);
+    } else if (a >= 65 && a <= 90) {
+        printf("%c is an uppercase letter.\n", a);
+    } else if (a >= 97 && a <= 122) {
+        printf("%c is an lowercase letter.\n", a);
+    } else {
+        printf("Something else.\n");
+    }
+
+    return 0;
+}
