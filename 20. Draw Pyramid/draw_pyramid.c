e91eecd7ab86abbb29c8babe1190a13425441bf9
sadman_ichha <sadmanichha1@gmail.com>
2023-09-14 09:39:49 +0600

    diamond

--- 20. Draw Pyramid/draw_pyramid.c
+++ 20. Draw Pyramid/draw_pyramid.c
@@ -3,25 +3,20 @@ int main()
 {
     int i, j, rows = 5;
 
-    for (i = 0; i < rows; i++)
-    {
-        for (j = 0; j <= i; j++)
-        {
+    for (i = 0; i < rows; i++){
+        for (j = 0; j <= i; j++){
             printf("* ");
         }
         printf("\n");
     }
 
     // Full pyramid
-    for (i = 0; i < rows; i++)
-    {
-        for (j = i; j < rows; j++)
-        {
+    for (i = 0; i < rows; i++){
+        for (j = i; j < rows; j++){
             printf(" ");
         }
 
-        for (j = 0; j < (2 * i) - 1; j++)
-        {
+        for (j = 0; j < (2 * i) - 1; j++){
             printf("*");
         }
         printf("\n");
