a192fe563dd62ba768572fd765a0e3bf577b520f
sadman_ichha <sadmanichha1@gmail.com>
2023-10-29 00:37:38 +0600

    operators priority

--- Practices_01/09. Type Conversion/type_conversion.c
+++ Practices_01/09. Type Conversion/type_conversion.c
@@ -6,6 +6,9 @@ int main() {
     printf("%c is the character itself\n", ch); // Print the character 'A'
     printf("%d is the decimal ASCII value\n", ch); // Print the decimal ASCII value of 'A'
     printf("%x is the hexadecimal ASCII value\n", ch); // Print the hexadecimal ASCII value of 'A'
+    
+    int x = 4/2+8*4-(5+2)%3;
+    printf("x = %d\n", x);
 
     return 0;
 }
