42c6615767a15bfbac841f6f05450600209d10e7
sadman_ichha <sadmanichha1@gmail.com>
2023-11-20 23:59:22 +0600

    arreys  minimum

---
+++ Practices_02/23. Pattren/02. Pattren Type 2/type_1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+// 1 2 3 4 5
+// 1 2 3 4
+// 1 2 3
+// 1 2
+// 1
+int main()
+{
+    int num, row, column;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    // Loop to print the pattern
+    for (row = num; row >= 1; row--)
+    {
+        for (column = 1; column <= row; column++)
+        {
+            printf("%d ", column); // Print the specified character
+        }
+        printf("\n"); // Move to the next line after each row
+    }
+
+    return 0;
+}
