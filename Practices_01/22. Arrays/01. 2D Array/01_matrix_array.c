91165b67ad90a4476f0e7738f1c9288790ea7fd1
sadman_ichha <sadmanichha1@gmail.com>
2023-11-22 22:13:26 +0600

    addition_2d array

--- Practices_01/22. Arrays/01. 2D Array/01_matrix_array.c
+++ Practices_01/22. Arrays/01. 2D Array/01_matrix_array.c
@@ -2,20 +2,31 @@
 
 int main()
 {
-    // Declaring and initializing a 3x2 matrix
-    int matrix[3][2] = {
-        {10, 20},
-        {30, 40},
-        {50, 60},
-    };
+    // Declaring Here [3] is Row..[2] is Column.. and initializing a 3x2 matrix.....
+    // int matrix[3][2] = {
+    //     {10, 20},
+    //     {30, 40},
+    //     {50, 60},
+    // };
+
+    int matrix[3][2];
+    int row, column;
 
     // Calculating the number of rows in the matrix
     int rows_length = sizeof(matrix) / sizeof(matrix[0]); // Number of rows
     // Calculating the number of columns in the matrix
     int columns_length = sizeof(matrix[0]) / sizeof(matrix[0][0]); // Number of columns
 
-    int row, column;
-
+    for (row = 0; row < 3; row++)
+    {
+        for (column = 0; column < 2; column++)
+        {
+            printf("Enter 2D array Rows 3 and Column 2 : ");
+            scanf("%d", &matrix[row][column]);
+        }
+    }
+    printf("\n");
+    printf("2D array  : \n");
     // Looping through each row of the matrix
     for (row = 0; row < rows_length; row++)
     {
