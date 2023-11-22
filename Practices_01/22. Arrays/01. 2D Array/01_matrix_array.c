b8900e39d6fa44407a48e74377e42acf5ee3c1b8
sadman_ichha <sadmanichha1@gmail.com>
2023-11-22 15:12:30 +0600

    matrix array 2d array

---
+++ Practices_01/22. Arrays/01. 2D Array/01_matrix_array.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main()
+{
+    // Declaring and initializing a 3x2 matrix
+    int matrix[3][2] = {
+        {10, 20},
+        {30, 40},
+        {50, 60},
+    };
+
+    // Calculating the number of rows in the matrix
+    int rows_length = sizeof(matrix) / sizeof(matrix[0]); // Number of rows
+    // Calculating the number of columns in the matrix
+    int columns_length = sizeof(matrix[0]) / sizeof(matrix[0][0]); // Number of columns
+
+    int row, column;
+
+    // Looping through each row of the matrix
+    for (row = 0; row < rows_length; row++)
+    {
+        // Looping through each column of the matrix
+        for (column = 0; column < columns_length; column++)
+        {
+            // Printing each element of the matrix
+            printf("%d ", matrix[row][column]); // Print the element
+        }
+        printf("\n"); // Move to the next line after printing each row
+    }
+    return 0;
+}
