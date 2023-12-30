1394fb2bf5cf3c42830c6196d34938e5af961def
sadman_ichha <sadmanichha1@gmail.com>
2023-12-30 11:29:10 +0600

    pointer intro

--- C/Practices_01/24.Pointer/00_intro_pointer.c
+++ C/Practices_01/24.Pointer/00_intro_pointer.c
@@ -9,7 +9,7 @@ int main()
     ptr_ch = &ch;
     printf("Orginal  : %c \n", ch);
     printf("From Ptr : %c \n", *ptr_ch);
-    printf("Ptr Address : %p ", *ptr_ch);
+    printf("Memory Address : %p ", *ptr_ch);
 
     // Double type pointer
     printf("\n \n---Double type pointer--- \n");
@@ -18,7 +18,7 @@ int main()
     ptr_cgpa = &cgpa;
     printf("Orginal  : %lf \n", cgpa);
     printf("From Ptr : %lf \n", *ptr_cgpa);
-    printf("Ptr Address : %p ", *ptr_cgpa);
+    printf("Memory Address : %p ", *ptr_cgpa);
 
     // Intger type pointer
     printf("\n \n---Intger type pointer--- \n");
@@ -27,6 +27,16 @@ int main()
     ptr_age = &age;
     printf("Orginal  : %d \n", age);
     printf("From Ptr : %d \n", *ptr_age);
-    printf("Ptr Address : %p ", *ptr_age);
+    printf("Memory Address : %p ", *ptr_age);
+
+    // Sum pointer
+    printf("\n \n---Sum pointer--- \n");
+    int num1 = 10, num2 = 40;
+    int *ptr_num1, *ptr_num2;
+    ptr_num1 = &num1;
+    ptr_num2 = &num2;
+    int sum;
+    sum = *ptr_num1 + *ptr_num2;
+    printf("Sum : %d ", sum);
     return 0;
 }
