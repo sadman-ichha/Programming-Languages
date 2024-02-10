838b67f4b5155354ff0d546c4c5401458e9c977a
sadman_ichha <sadmanichha1@gmail.com>
2024-02-10 17:27:26 +0600

    Added Dart: Hello World

--- Dart/Smart Dart/Hello World/hello_world.dart
+++ Dart/Smart Dart/Hello World/hello_world.dart
@@ -3,5 +3,5 @@ void main() {
   // You can add more code here to extend your program
   // For example:
   int sum = 5+7;
-  print('The sum of 5 and 7 is: $sum');
+  print('The sum of 5 and 7 is: $sum ');
 }
