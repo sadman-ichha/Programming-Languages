867c364e43eda7335a433d6f49abec762f234bad
sadman_ichha <sadmanichha1@gmail.com>
2024-04-29 10:58:35 +0600

    Added C++: Area Of Circle

--- C++/Learing-C++/23. Math/01_areaof_circle.cpp
+++ C++/Learing-C++/23. Math/01_areaof_circle.cpp
@@ -1,3 +1,7 @@
+/* The formula to calculate the area of a circumference is defined as A = π . R2. Considering to this problem that π = 3.14159:
+
+Calculate the area using the formula given in the problem description.*/
+
 #include <iostream>
 #include <iomanip> // Include the header file for fixed and setprecision
 using namespace std;
