de348cb1d3bf276196ae7bc692b5b0549c1c8b38
sadman_ichha <sadmanichha1@gmail.com>
2024-05-14 23:41:23 +0600

    Added C++: Simple product

---
+++ C++/Learing-C++/24. PROBLEM/03_simple_product.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int A, B, PROD;
+    cin >> A;
+    cin >> B;
+    PROD = A * B;
+    cout << "PROD = " << PROD << endl;
+    return 0;
+}
