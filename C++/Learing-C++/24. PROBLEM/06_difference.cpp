accd0492690db059d21d52a7bd5cef4a07ce5b04
sadman_ichha <sadmanichha1@gmail.com>
2024-05-16 10:51:10 +0600

    Added C++: Difference beecrowed

---
+++ C++/Learing-C++/24. PROBLEM/06_difference.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A, B, C, D, differnce;
+    cin >> A >> B >> C >> D;
+    differnce = (A * B) - (C * D);
+    cout << "DIFERENCA = " << differnce << endl;
+
+    return 0;
+}
---
+++ C++/Learing-C++/24. PROBLEM/07_salary.cpp
@@ -0,0 +1,14 @@
+// Write a program that reads an employee's number, his/her worked hours number in a month and the amount he received per hour. 
+// Print the employee's number and salary that he/she will receive at end of the month, with two decimal places.
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int employee,number;
+    float salary;
+    cin
+
+    return 0;
+}
