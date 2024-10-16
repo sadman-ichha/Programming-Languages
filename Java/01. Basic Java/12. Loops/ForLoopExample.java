6c788b6ceb9f66752a9f629726d1b13b1990e1ed
Sadman Ichha <sadmanichha1@gmail.com>
2024-10-16 09:15:20 +0600

    added:loop

---
+++ Java/01. Basic Java/12. Loops/ForLoopExample.java
@@ -0,0 +1,8 @@
+public class ForLoopExample {
+    public static void main(String[] args) {
+        int[] x={1,2,5,4,6,8,14,12,14};
+        for(int i=1; i<x.length; i +=1){
+            System.err.println(x[i]);
+        }
+    }
+}
