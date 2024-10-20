e9e906cc9a6ea0a16bd8a8b0c6375f310c1cd6b8
Sadman Ichha <sadmanichha1@gmail.com>
2024-10-20 09:53:53 +0600

    added: random

--- Java/01. Basic Java/13. Break Continue/Random.java
+++ Java/01. Basic Java/13. Break Continue/Random.java
@@ -2,6 +2,7 @@
 import java.util.Scanner;
 
 public class Random {
+    @SuppressWarnings("resource")
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number of terms you want in the fibbonachi series");
