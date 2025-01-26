2873f95c0ddf1acb5b058edd86755ca7779d2b40
Sadman Ichha <sadmanichha1@gmail.com>
2025-01-26 21:24:06 +0600

    added

---
+++ Java/03. File Headling/Directory.java
@@ -0,0 +1,10 @@
+import java.io.File;
+
+public class Directory {
+
+    public static void main(String args[]){
+        File mkFile =new File("Sadman");
+
+    }
+    
+}
--- python/main.py
+++ python/main.py
@@ -1,12 +1,6 @@
-print("Hello World!")
+x = "hello"
+y = "world"
+x,y = y,x
+print(x,y)
 
-print(2+2)
 
-x = 45
-print(type(x))
-
-y,z,a="Sadmannn","Ichha", "Jone"
-
-print(y,z,a)
-print("Display")
-print(a)
