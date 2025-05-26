e5999b44a4bd01984bdd75a44656a46b71bd779d
Sadman Ichha <sadmanichha1@gmail.com>
2025-05-26 10:08:40 +0600

    go added: packages

---
+++ go.mod
@@ -0,0 +1,3 @@
+module programming-languages/go_lang/03-packages-and-scopes/01-packages/myapp
+
+go 1.22.2
---
+++ go_lang/03-packages-and-scopes/01-packages/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+	"01-package-folder/hello" // module_name/subfolder
+)
+
+func main() {
+	fmt.Println("Print anything!")
+	hello.Hello()
+}
